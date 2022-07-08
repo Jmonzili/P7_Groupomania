78d878ac28b84fdbd1030d5149edd44c2c0c6667
Jeffrey Monzili <jeffrey.monzili@gmail.com>
2022-07-08 10:28:38 +0200

    Crud post

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -3,18 +3,59 @@ const PostModel = require('../models/post.model');
 const UserModel = require('../models/user.model');
 const ObjectID = require('mongoose').Types.ObjectId;
 
+//  Lire les posts
 module.exports.readPost = (req, res) => {
-   
+    PostModel.find((err, docs) => {
+        if (!err) res.send(docs);
+        else console.log('Error to get data :', err);
+    }).sort({ createdAt: -1 });
 };
 
+//  Créer un post
 module.exports.createPost = async (req, res) => {
-    
+    const newPost = new PostModel( {
+        posterId: req.body.posterId,
+        message: req.body.message,
+        video: req.body.video,
+        likers: [],
+        comments: []
+    });
+
+    try {
+        const post = await newPost.save();
+        return res.status(201).json(post);
+    } catch {
+        return res.status(400).send({ err });
+    }
 }
 
+//  Modifier un post
 module.exports.updatePost = (req, res) => {
-   
+    if (!ObjectID.isValid(req.params.id))
+      return res.status(400).send('ID unknown : ' + req.params.id);
+    
+    const updatedRecord = {
+        message: req.body.message
+    }
+
+    PostModel.findByIdAndUpdate(
+        req.params.id,
+        { $set: updatedRecord },
+        { new: true },
+        (err, docs) => {
+            if (!err) res.send(docs);
+            else console.log("Update error :", err);
+        }
+    )
 }
 
+//  Supprimer un post
 module.exports.deletePost = (req, res) => {
+    if (!ObjectID.isValid(req.params.id))
+      return res.status(400).send('ID unknown : ' + req.params.id);
     
+    PostModel.findByIdAndDelete(req.params.id,(err, docs) => {
+        if (!err) res.send(docs);
+        else console.log("Delete error :", err);
+    })
 }
